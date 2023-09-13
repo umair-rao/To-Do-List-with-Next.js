997f6361602ea1ebb2f93a5a3e3086baa5d1e0c5
Umair Rao <umairmuqeem@gmail.com>
2023-09-13 11:38:31 +0500

    Setup Redux

---
+++ app/Redux/store.js
@@ -0,0 +1,12 @@
+'use client';
+
+import { configureStore } from '@reduxjs/toolkit'; 
+import rootReducer from './reducer';
+
+export const store = configureStore({
+  reducer: {
+    tasks: rootReducer,
+  },
+});
+
+export default store;
